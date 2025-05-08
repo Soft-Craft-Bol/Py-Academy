a7a5e16cdca49fb287ae7801b5c8ce893ab3c3a5
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-05-07 20:08:28 -0400

    feat: configure all project

---
+++ front-pyAcademy/src/shared/api/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const baseURL = "http://localhost:8888/api/v1";
+
+const api = axios.create({
+    baseURL: baseURL,
+    responseType: 'json',
+    withCredentials: true, 
+    timeout: 60000,
+  });
+
+api.interceptors.request.use((config) => {
+  if (config.data instanceof FormData) {
+    config.headers['Content-Type'] = 'multipart/form-data';
+  } else {
+    config.headers['Content-Type'] = 'application/json';
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
+
+export const loginUser = (data) => api.post('/auth/log-in', data);
+export const addUser = (data) => api.post('/auth/sign-up', data);
+export const saludo = (nombre = "Usuario") => api.get('/saludo', { 
+    params: { nombre } 
+  });
