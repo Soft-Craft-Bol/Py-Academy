75e8b65614d02434ff362864d6bf505c2dcae9c9
link200309 <jonhh.0023@gmail.com>
2025-05-13 00:59:05 -0400

    feat: footer

--- front-pyAcademy/src/App.jsx
+++ front-pyAcademy/src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import "./App.css";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { Navbar } from "./shared/ui/organisms/Navbar";
+import { Footer } from "./shared/ui/organisms/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </>
   );
 }
