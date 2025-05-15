690053ce7e9f688d71e80f78daf6ff5673470836
RodriC98374 <rodricesp.3.0@gmail.com>
2025-05-15 17:17:07 -0400

    feat: Vista responsiva en gestion de cursos y home

--- front-pyAcademy/src/pages/home/components/HeroSection.jsx
+++ front-pyAcademy/src/pages/home/components/HeroSection.jsx
@@ -5,26 +5,27 @@ import heroImage from "../../../assets/heroImage.png";
 const HeroSection = () => {
   return (
     <section
-      className="relative flex items-center w-full px-12 py-13 h-screen justify-center overflow-hidden"
+      className="relative flex flex-col lg:flex-row items-center w-full px-6 md:px-12 py-10 md:py-13 h-auto lg:h-screen justify-center overflow-hidden"
       data-testid="hero-section"
     >
-      <div className="w-[50%]">
-        <h1 className="text-display-lg w-96 font-bold">Domina python con IA</h1>
-        <p className="w-[450px] my-6 text-title-sm">
+      <div className="w-full lg:w-1/2 flex flex-col items-center lg:items-start text-center lg:text-left mb-10 lg:mb-0">
+        <h1 className="text-3xl md:text-5xl font-bold max-w-md">
+          Domina python con IA
+        </h1>
+        <p className="max-w-lg mt-4 mb-8 text-base md:text-lg text-gray-700">
           Descubre una forma revolucionaria de aprender Python con nuestra
-          plataforma impulsada por IA y recursos educativos abiertos..
+          plataforma impulsada por IA y recursos educativos abiertos.
         </p>
-        <div>
-          <Button size="lg" onClick={() => console.log("Registrarse")}>
-            Comenzar ahora
-          </Button>
-        </div>
+        <Button size="lg" onClick={() => console.log("Registrarse")}>
+          Comenzar ahora
+        </Button>
       </div>
-      <div className="w-[573px] h-[382px] rounded-[20px] overflow-hidden shadow-blue-500/50 shadow-xl scale-100 hover:scale-105 transition-transform duration-300 ease-in-out">
+
+      <div className="w-full lg:w-[573px] h-auto max-h-[400px] rounded-[20px] overflow-hidden shadow-blue-500/50 shadow-xl transition-transform duration-300 ease-in-out hover:scale-105">
         <img
           src={heroImage}
           alt="Estudiante programando"
-          className="w-full h-full object-cover scale-100"
+          className="w-full h-full object-cover"
         />
       </div>
     </section>
