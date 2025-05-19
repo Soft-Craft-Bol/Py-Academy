8244d50e19348356211906dce90a4b5dfa586683
link200309 <jonhh.0023@gmail.com>
2025-05-18 22:20:05 -0400

    fix(CodeEditorPage): Mode dark, update(CodeEditorPage): Styles

--- front-pyAcademy/src/pages/student/CodeEditorPage.jsx
+++ front-pyAcademy/src/pages/student/CodeEditorPage.jsx
@@ -4,35 +4,49 @@ import Button from "../../shared/ui/atoms/Button";
 //React
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import { IoIosColorPalette } from "react-icons/io";
 
 const CodeEditorPage = () => {
   const [code, setCode] = useState("print('Hola mundo')");
   const [output, setOutput] = useState("");
+  const [themeEditor, setThemeEditor] = useState("vs");
 
   const handleExecuteCode = (value) => {
     console.log("Ejecutar código", value);
   };
 
+  const handleThemeEditor = () => {
+    if (themeEditor == "vs") setThemeEditor("vs-dark");
+    if (themeEditor == "vs-dark") setThemeEditor("hc-black");
+    if (themeEditor == "hc-black") setThemeEditor("vs");
+  };
+
   return (
     <div className="mt-10 mx-auto w-[90%]">
-      <div className="dark:bg-primary-pri1 p-7 rounded-lg">
+      <div className="bg-primary-pri2 dark:bg-primary-pri1 p-7 rounded-lg shadow-blue-500/50 shadow-lg">
         <div className="flex justify-between items-center mb-4">
-          <h1 className="p-1 rounded-lg text-center font-bold text-title-md">
-            Simulador visual del comportamiento del código
+          <h1 className="p-1 rounded-lg text-center font-bold text-title-md text-white">
+            Editor de codigo Python
           </h1>
-          <Button
-            variant="primary"
-            size="large"
-            onClick={() => {
-              console.log("Ejecutar");
-            }}
-            className="bg-primary-pri3 px-4 py-2"
-          >
-            Ejecutar
-          </Button>
+
+          <div className="flex gap-5 items-center">
+            <IoIosColorPalette
+              className="text-3xl text-primary-pri4"
+              onClick={handleThemeEditor}
+            />
+
+            <Button
+              variant="primary"
+              size="large"
+              onClick={handleExecuteCode}
+              className="bg-primary-pri3 px-4 py-2"
+            >
+              Ejecutar
+            </Button>
+          </div>
         </div>
 
-        <h2 className="font-semibold my-4 text-label-md">Editor de Python</h2>
+        <h2 className="font-semibold my-4 text-label-md text-white">Entrada</h2>
 
         <div>
           <Editor
@@ -40,8 +54,11 @@ const CodeEditorPage = () => {
             language="python"
             value={code}
             onChange={(value) => setCode(value)}
+            theme={themeEditor}
           />
-          <button onClick={handleExecuteCode}>Ejecutar</button>
+          <h2 className="font-semibold my-4 text-label-md text-white">
+            Salida
+          </h2>
           <pre style={{ background: "#eee", padding: "1em" }}>{output}</pre>
         </div>
       </div>
