76163fa439eec1ae3be6e2d460309c183b6c2d49
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-05-24 16:26:00 -0400

    feat: implement authentication context and login functionality

--- front-pyAcademy/src/App.jsx
+++ front-pyAcademy/src/App.jsx
@@ -2,24 +2,27 @@ import Home from "./pages/home/Home";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ManageCourses from "./pages/ManageCourses/ManageCourses";
-import Login from "./pages/login/Login";
+//import Login from "./pages/login/Login";
 import { StudentLayout } from "./shared/layouts/StudentLayout";
 import { TeacherLayout } from "./shared/layouts/TeacherLayout";
 import { PublicLayout } from "./shared/layouts/PublicLayout";
 import CoursesPage from "./pages/student/CoursesPage";
 import CodeEditorPage from "./pages/student/CodeEditorPage";
 import { ChatIA } from "./pages/student/ChatIA";
+import LoginPage from "./pages/auth/LoginPage";
+import { AuthProvider } from "./app/context/AuthContext";
 
 function App() {
   return (
     <>
+    <AuthProvider>
       <Routes>
         <Route element={<PublicLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/explorar-cursos" element={<Home />} />
           <Route path="/Recursos-OER" element={<Home />} />
           <Route path="/ia-tutor" element={<Home />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginPage />} />
         </Route>
 
         <Route element={<PublicLayout />}>
@@ -32,6 +35,7 @@ function App() {
           <Route path="chatIA" element={<ChatIA />} />
         </Route>
       </Routes>
+      </AuthProvider>
     </>
   );
 }
