a48285aa774d202778f7fd6866036aff3f54cccc
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-06-10 12:26:07 -0400

    feat: config routes

--- front-pyAcademy/src/App.jsx
+++ front-pyAcademy/src/App.jsx
@@ -1,42 +1,9 @@
-import React from "react";
-import Home from "./pages/home/Home";
+// src/App.jsx
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import ManageCourses from "./pages/ManageCourses/ManageCourses";
-import ExplorateCourses from "./pages/ExplorateCourses/ExplorateCourses";
-import PrivateCourseView from "./pages/ExplorateCourses/PrivateCourseView";
-//import Login from "./pages/login/Login";
-import { StudentLayout } from "./shared/layouts/StudentLayout";
-import { TeacherLayout } from "./shared/layouts/TeacherLayout";
-import { PublicLayout } from "./shared/layouts/PublicLayout";
-import CoursesPage from "./pages/student/CoursesPage";
-import { ChatIA } from "./pages/student/ChatIA";
-import LoginPage from "./pages/auth/LoginPage";
-import PyEditor from "./pages/student/PyEditorPage";
+import { AppRoutes } from "./app/routes/AppRoutes";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route element={<PublicLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/explorar-cursos" element={<ExplorateCourses />} />
-          <Route path="/Recursos-OER" element={<Home />} />
-          <Route path="/ia-tutor" element={<Home />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/gestionar-cursos" element={<ManageCourses />} />
-          <Route path="/curso/:id" element={<PrivateCourseView />} />
-        </Route>
-
-        <Route element={<TeacherLayout />}></Route>
-
-        <Route path="/student" element={<StudentLayout />}>
-          <Route index element={<CoursesPage />} />
-          <Route path="editor" element={<PyEditor />} />
-          <Route path="chatIA" element={<ChatIA />} />
-        </Route>
-      </Routes>
-    </>
-  );
+  return <AppRoutes />;
 }
+
 export default App;
