eb31706a629d7b379fcb7bc9e7d98686facb9af8
RodriC98374 <rodricesp.3.0@gmail.com>
2025-06-22 19:00:52 -0400

    fix: Resolución de conflictos y variables duplicadas

--- front-pyAcademy/src/pages/student/Certificates.jsx
+++ front-pyAcademy/src/pages/student/Certificates.jsx
@@ -5,7 +5,8 @@ import python_basico from '@/assets/ManageCourses/python_basico.jpg';
 import EjemploFirma from '@/assets/img/EjemploFirma.png';
 
 import { useLocation, useNavigate } from 'react-router-dom';
-import python_basico from '@/assets/ManageCourses/python_basico.jpg';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 const mockCompletedCourses = [
   {
