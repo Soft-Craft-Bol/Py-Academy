900c2c0303eb83f591d0cf61b8d61f13cc45417e
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-06-29 16:39:15 -0400

    fix: update list courses a public

--- front-pyAcademy/src/shared/api/api.js
+++ front-pyAcademy/src/shared/api/api.js
@@ -48,10 +48,19 @@ export const inscribirseCurso = (data) => api.post('/courses/enrollments', data)
 
 //create course
 //http://localhost:8888/api/v1/courses?teacherId=1
-export const createCourse = (formData) => {
-  return api.post('/courses', formData, {
+export const createCourse = (formData) => {return api.post('/courses', formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
     },
   });
 };
+//con paginacion
+
+export const getAllCourses = (page = 0, size = 10) => {
+  return api.get('/courses/all', {
+    params: {
+      page: page,
+      size: size,
+    },
+  });
+}
