c7b1a290a0bde38ebe7b38e6d0c691e7eb49202f
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-06-29 13:54:43 -0400

    fix: update curser

--- backend/src/main/java/com/pyAcademy/pyAcademy/features/exercises/application/usecase/CreateExerciseUseCase.java
+++ backend/src/main/java/com/pyAcademy/pyAcademy/features/exercises/application/usecase/CreateExerciseUseCase.java
@@ -11,7 +11,7 @@ import java.sql.Timestamp;
 public class CreateExerciseUseCase {
 
     private final SaveExercisePort saveExercisePort;
-
+//jaja
     public CreateExerciseUseCase(SaveExercisePort saveExercisePort) {
         this.saveExercisePort = saveExercisePort;
     }
