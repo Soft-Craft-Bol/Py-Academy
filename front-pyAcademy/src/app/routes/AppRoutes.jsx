d82e3e49287642c765bf7cab80960c9316e6e82a
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2025-06-30 13:16:54 -0400

    fix: changes

--- front-pyAcademy/src/app/routes/AppRoutes.jsx
+++ front-pyAcademy/src/app/routes/AppRoutes.jsx
@@ -39,6 +39,7 @@ const CreatePracticePage = lazy(() => import('@/pages/teacher/CreatePracticePage
 const CourseDetailsPage = lazy(() => import('@/pages/teacher/CourseDetailsPage'));
 const TeacherCoursesPage = lazy(() => import('@/pages/teacher/components/TeacherCoursesPage'));
 const EditCoursePage = lazy(() => import('../../shared/layouts/EditCoursePage'));
+const CourseStudentsPage = lazy(() => import('../../pages/teacher/CourseDetailsPage'));
 
 export function AppRoutes() {
   return (
@@ -66,6 +67,7 @@ export function AppRoutes() {
             <Route path="create-course" element={<CourseManangement />} />
             <Route path="learning-units" element={<LearningUnitsManager />} />
             <Route path="gestionar-cursos/edit/:id" element={<EditCoursePage />} />
+            <Route path="studentList/:id/" element={<CourseStudentsPage />} />
           </Route>
         </Route>
 
